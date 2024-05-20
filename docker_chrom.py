063bc4e7caab619903799fb5585728301a4ced2c
Hello_123 <163098764+content1234@users.noreply.github.com>
2024-05-20 13:23:05 +0800

    Update docker_chrom.py

--- docker_chrom.py
+++ docker_chrom.py
@@ -1,14 +1,17 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 
-driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=chrome_options.to_capabilities())
+# driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=chrome_options.to_capabilities())
+wd = webdriver.Chrome(options=chrome_options)
+url = "https://www.baidu.com/"
+wd.get(url=url)
 
-driver.get("https://www.baidu.com")
-
-print(driver.title)
-
-driver.quit()
+time.sleep(30)
+data = wd.page_source
+# print(data)
+wd.quit()
