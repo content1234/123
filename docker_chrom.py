9480552fc4f9587bd6c1249ee05545ca43aee511
Hello_123 <163098764+content1234@users.noreply.github.com>
2024-05-20 14:28:03 +0800

    Update docker_chrom.py

--- docker_chrom.py
+++ docker_chrom.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 
 # driver = webdriver.Remote(command_executor='http://localhost:4444/wd/hub', desired_capabilities=chrome_options.to_capabilities())
